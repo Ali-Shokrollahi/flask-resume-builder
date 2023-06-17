1d05f28b3eee30f6b2857c873b68af1933201951
ali.sh <ali.shokrollahi.pro@gmail.com>
2023-06-17 12:14:13 +0330

    create 'accounts' blueprint.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from app.extensions import db, migrate
+from app.accounts.views import blueprint as account_blueprint
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(account_blueprint)
+
 
 app = Flask(__name__)
 app.config.from_object('config.DevConfig')
